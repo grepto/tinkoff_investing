b2c7ef1cd84360e6f08b416e12317ee3d6f9c967
Alexandr Popov <grepto@gmail.com>
2019-11-26 16:56:55 +0300

    add get_yesterday_market_result and tests

--- src/tinkoff_invest.py
+++ src/tinkoff_invest.py
@@ -1,9 +1,12 @@
 import sys
+from collections import defaultdict
 from datetime import datetime, timedelta
 
 from envparse import env
 from openapi_client import openapi
 from pytz import UTC
+
+from src.helpers import get_percentage_diff, short_name
 
 SECONDS_PER_DAY = 86400
 
@@ -64,5 +67,52 @@ def get_instrument_change(figi: str, _from: datetime, to: datetime) -> dict:
     return dict(open=open, close=close, lo=lo, hi=hi, vol=vol)
 
 
+def get_yesterday_market_result(curency: str) -> dict:
+    portfolio = list(
+        filter(lambda x: x['instrument_type'] == 'Stock' and x['average_position_price']['currency'] == curency,
+               get_portfolio()))
+
+    yesterday = datetime.now(UTC) - timedelta(days=1)
+
+    _from = yesterday.replace(hour=0, minute=0, second=0, microsecond=1)
+    to = yesterday.replace(hour=23, minute=59, second=59, microsecond=0)
+
+    instruments = []
+
+    for instrument in portfolio:
+        name = get_instrument(instrument['figi'])['name']
+        close_price = get_orderbook(instrument['figi'], 1)['close_price']
+        average_price = instrument['average_position_price']['value']
+        balance = instrument['balance']
+        change = get_instrument_change(instrument['figi'], _from, to)
+        expected_yield = instrument['expected_yield']['value']
+
+        instruments.append(dict(name=short_name(name),
+                                close_price=close_price,
+                                average_price=average_price,
+                                balance=balance,
+                                yesterday_change=round(change['close'] - change['open'], 2),
+                                yesterday_change_percentage=round(get_percentage_diff(change['open'], change['close']),
+                                                                  3),
+                                expected_yield=expected_yield,
+                                expected_yield_percentage=round(expected_yield / (average_price * balance), 3),
+                                )
+                           )
+
+    totals = defaultdict(float)
+    for instrument in instruments:
+        totals['yesterday_change'] += instrument['yesterday_change']
+        totals['yesterday_change_percentage'] += instrument['yesterday_change_percentage']
+        totals['expected_yield'] += instrument['expected_yield']
+        totals['expected_yield_percentage'] += instrument['expected_yield_percentage']
+
+    totals['yesterday_change_percentage'] /= len(instruments)
+    totals['expected_yield_percentage'] /= len(instruments)
+
+    totals = {key: round(value, 3 if '_percentage' in key else 2) for (key, value) in totals.items()}
+
+    return dict(totals=totals, instruments=instruments)
+
+
 if __name__ == '__main__':
-    figi = 'BBG004S686N0'  # BANEP
+    pass
--- tests/test_tinkoff_invest.py
+++ tests/test_tinkoff_invest.py
@@ -4,7 +4,39 @@ import pytest
 from freezegun import freeze_time
 from pytz import UTC
 
-from src.tinkoff_invest import get_yesterday_candle, get_instrument_change
+from src.tinkoff_invest import get_yesterday_candle, get_instrument_change, get_yesterday_market_result
+
+portfolio = [
+    {'figi': 'BBG004S686N0', 'ticker': 'BANEP', 'isin': 'RU0007976965', 'instrument_type': 'Stock', 'balance': 49.0,
+     'blocked': None, 'lots': 49, 'expected_yield': {'currency': 'RUB', 'value': 4140.5},
+     'average_position_price': {'currency': 'RUB', 'value': 1656.5}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG004730RP0', 'ticker': 'GAZP', 'isin': 'RU0007661625', 'instrument_type': 'Stock', 'balance': 210.0,
+     'blocked': None, 'lots': 21, 'expected_yield': {'currency': 'RUB', 'value': 7128.1},
+     'average_position_price': {'currency': 'RUB', 'value': 220.86}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG0047315Y7', 'ticker': 'SBERP', 'isin': 'RU0009029557', 'instrument_type': 'Stock', 'balance': 510.0,
+     'blocked': None, 'lots': 51, 'expected_yield': {'currency': 'RUB', 'value': 9129.0},
+     'average_position_price': {'currency': 'RUB', 'value': 196.6}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG000BY2Y78', 'ticker': 'IVZ', 'isin': 'BMG491BT1088', 'instrument_type': 'Stock', 'balance': 13.0,
+     'blocked': 13.0, 'lots': 13, 'expected_yield': {'currency': 'USD', 'value': 3.51},
+     'average_position_price': {'currency': 'USD', 'value': 17.48}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG005DXJS36', 'ticker': 'TCS', 'isin': 'US87238U2033', 'instrument_type': 'Stock', 'balance': 11.0,
+     'blocked': None, 'lots': 11, 'expected_yield': {'currency': 'USD', 'value': -11.88},
+     'average_position_price': {'currency': 'USD', 'value': 19.9}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG00F0JX6G7', 'ticker': 'RU000A0JWWG0', 'isin': 'RU000A0JWWG0', 'instrument_type': 'Bond',
+     'balance': 127.0, 'blocked': None, 'lots': 127, 'expected_yield': {'currency': 'RUB', 'value': 223.52},
+     'average_position_price': {'currency': 'RUB', 'value': 798.9},
+     'average_position_price_no_nkd': {'currency': 'RUB', 'value': 786.08}},
+    {'figi': 'BBG00GK399S6', 'ticker': 'RU000A0JXQ93', 'isin': 'RU000A0JXQ93', 'instrument_type': 'Bond',
+     'balance': 37.0, 'blocked': None, 'lots': 37, 'expected_yield': {'currency': 'RUB', 'value': 217.98},
+     'average_position_price': {'currency': 'RUB', 'value': 879.35},
+     'average_position_price_no_nkd': {'currency': 'RUB', 'value': 871.07}},
+    {'figi': 'BBG005DXDPK9', 'ticker': 'FXGD', 'isin': 'IE00B8XB7377', 'instrument_type': 'Etf', 'balance': 184.0,
+     'blocked': None, 'lots': 184, 'expected_yield': {'currency': 'RUB', 'value': -2403.2},
+     'average_position_price': {'currency': 'RUB', 'value': 657.2}, 'average_position_price_no_nkd': None},
+    {'figi': 'BBG0013HGFT4', 'ticker': 'USD000UTSTOM', 'isin': None, 'instrument_type': 'Currency', 'balance': 2.83,
+     'blocked': 0.15, 'lots': 0, 'expected_yield': {'currency': 'RUB', 'value': -1.84},
+     'average_position_price': {'currency': 'RUB', 'value': 64.66}, 'average_position_price_no_nkd': None}
+]
 
 candels = [
     {'figi': 'BBG0047315Y7', 'interval': 'day', 'o': 214.55, 'c': 214.98, 'h': 215.99, 'l': 213.79, 'v': 298619,
@@ -16,6 +48,16 @@ candels = [
 @pytest.fixture
 def get_candles(mocker):
     return mocker.patch('src.tinkoff_invest.get_candles')
+
+
+@pytest.fixture
+def get_portfolio(mocker):
+    return mocker.patch('src.tinkoff_invest.get_portfolio')
+
+
+@pytest.fixture
+def instrument_change_result(mocker):
+    return mocker.patch('src.tinkoff_invest.get_instrument_change')
 
 
 @freeze_time("2019-11-14 15:48:11")
@@ -68,3 +110,30 @@ def test_get_instrument_change_result(get_candles):
         hi=max(candels, key=lambda x: x['h'])['h'],
         vol=sum([candle['v'] for candle in candels])
     )
+
+
+@pytest.mark.parametrize(
+    'currency, expected_portfolio_len',
+    [
+        pytest.param('RUB', 3, id='RUB instruments'),
+        pytest.param('USD', 2, id='USD instruments'),
+    ]
+)
+def test_get_yesterday_market_result_portfolio_currency_filtering(get_portfolio, currency, expected_portfolio_len):
+    get_portfolio.return_value = portfolio
+    yesterday_market_result = get_yesterday_market_result(currency)['instruments']
+
+    assert len(yesterday_market_result) == expected_portfolio_len
+
+
+@freeze_time("2019-11-14 15:48:11")
+def test_get_yesterday_market_result_from_to_dates(get_portfolio, instrument_change_result):
+    get_portfolio.return_value = portfolio[:1]
+    instrument_change_result.return_value = dict(open=1, close=2, lo=3, hi=4, vol=5)
+    get_yesterday_market_result('RUB')
+
+    yesterday = datetime.now(UTC) - timedelta(days=1)
+    _from = yesterday.replace(hour=0, minute=0, second=0, microsecond=1)
+    to = yesterday.replace(hour=23, minute=59, second=59, microsecond=0)
+
+    instrument_change_result.assert_called_with(portfolio[:1][0]['figi'], _from, to)
